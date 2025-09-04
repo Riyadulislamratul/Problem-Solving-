1d832b256b9358300650a36cefff4b67ff740cbc
Md Riyadul Islam Ratul <riadul1810833319@gmail.com>
2025-09-05 00:45:41 +0600

    Add files via upload

---
+++ multiplicationTable.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,mul;
+    scanf("%d",&n);
+    for(i=1;i<11;i++)
+    {
+        mul=i*n;
+        printf("%d x %d = %d\n",i,n,mul);
+    }
+    return 0;
+}
